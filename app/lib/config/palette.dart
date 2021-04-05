e720f6d652356e056e948ae0f5ffdafb6314c5d4
princekumargupta <60013997+princekumargupta@users.noreply.github.com>
2021-04-05 22:56:12 +0530

    added UI templates

---
+++ app/lib/config/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static const Color darkBlue = Color(0xff001524);
+  static const Color lightBlue = Color(0xff15616D);
+  static const Color orange = Color(0xffFF7D00);
+  static const Color darkOrange = Color(0xffB85900);
+}

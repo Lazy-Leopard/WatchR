7b2a4a82cfb580b7e571c6d0432fa81758ec1140
Shourya <58784199+lazyp4nd4@users.noreply.github.com>
2021-04-06 13:41:21 +0530

    Implemented Censys API connection with Flutter

---
+++ app/lib/ip-locator/ipadd.dart
@@ -0,0 +1,13 @@
+class IPAdd {
+  // ignore: non_constant_identifier_names
+  String city, country, service_provider, ip;
+  double lat, lang;
+  IPAdd(
+      {this.city,
+      this.country,
+      this.ip,
+      this.lang,
+      this.lat,
+      // ignore: non_constant_identifier_names
+      this.service_provider});
+}
---
+++ app/lib/ip-locator/listOfIpSearches.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:osint/config/palette.dart';
+import 'package:osint/ip-locator/ipSearchTile.dart';
+import 'package:osint/ip-locator/ipadd.dart';
+import 'package:provider/provider.dart';
+
+class IPSearchList extends StatefulWidget {
+  @override
+  _IPSearchListState createState() => _IPSearchListState();
+}
+
+class _IPSearchListState extends State<IPSearchList> {
+  @override
+  Widget build(BuildContext context) {
+    final ips = Provider.of<List<IPAdd>>(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        title: Text(
+          'Previous IPs Located',
+          style:
+              TextStyle(color: Palette.darkOrange, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 8),
+        child: ListView.builder(
+          itemCount: ips.length,
+          itemBuilder: (context, index) {
+            return IPSearchTile(
+              ipadd: ips[index],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
